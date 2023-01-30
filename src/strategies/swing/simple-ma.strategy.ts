568fce6ad3cc1342b15e02885ce998cba9604c9c
Nathan Adams <npadams21@gmail.com>
2023-01-30 18:56:13 +0000

    add: cci indicator to  canTraxe bool

--- src/strategies/swing/simple-ma.strategy.ts
+++ src/strategies/swing/simple-ma.strategy.ts
@@ -5,40 +5,40 @@ import { Strategy } from '../../model/strategy';
 import { BaseStrategy } from '../base-strategy';
 
 export class SimpleMAStrategy extends BaseStrategy {
-    ewo: Indicator;
+  cci: Indicator;
 
-    constructor(public strat: Strategy) {
-        super(strat);
-        this.strategyName = 'Swing EWO MA Strategy';
+  constructor(public strat: Strategy) {
+    super(strat);
+    this.strategyName = 'Swing EWO MA Strategy';
+  }
+
+  loadIndicators() {
+    this.cci = addIndicator('sniper-cci', { weight: 14, weights: [5, 35], name: 't3-cci' });
+  }
+
+  async realtimeAdvice() {}
+
+  async advice() {
+    this.checkTradeStatus(() => {
+      return this.cci.result < 0;
+    });
+
+    if (this.tradeAdvisor.actionType === ActionType.Long && !this.delayOn && this.canTrade) {
+      if (this.ema20.result > this.sma50.result) {
+        this.tradeAdvisor.trade();
+      }
     }
-
-    loadIndicators() {
-        this.ewo = addIndicator('ewo', { weight: 14, weights: [5, 35], name: 'ewo' });
+    if (this.tradeAdvisor.actionType === ActionType.Short) {
+      if (this.ema20.result < this.sma50.result) {
+        this.tradeAdvisor.trade();
+      }
     }
+  }
 
-    async realtimeAdvice() {}
-
-    async advice() {
-        this.checkTradeStatus(() => {
-            return this.ewo.result < 0;
-        });
-
-        if (this.tradeAdvisor.actionType === ActionType.Long && !this.delayOn && this.canTrade) {
-            if (this.ema20.result > this.sma50.result) {
-                this.tradeAdvisor.trade();
-            }
-        }
-        if (this.tradeAdvisor.actionType === ActionType.Short) {
-            if (this.ema20.result < this.sma50.result) {
-                this.tradeAdvisor.trade();
-            }
-        }
-    }
-
-    logStatus(advice: any): void {
-        let enabled = this.canTrade ? 'YES' : 'NO';
-        const usedIndicators = this.logger.displayIndicators();
-        let message = `${this.ticker.pair} PRICE: ${this.candle.price}. Enabled: ${enabled}. Strategy: ${usedIndicators} Profit: ${advice}`;
-        this.consoleColour(message);
-    }
+  logStatus(advice: any): void {
+    let enabled = this.canTrade ? 'YES' : 'NO';
+    const usedIndicators = this.logger.displayIndicators();
+    let message = `${this.ticker.pair} PRICE: ${this.candle.price}. Enabled: ${enabled}. Strategy: ${usedIndicators} Profit: ${advice}`;
+    this.consoleColour(message);
+  }
 }
