0220df5bd25e6acb656c89b869b0d76b0bc04f28
Nathan Adams <npadams21@gmail.com>
2023-02-03 17:57:11 +0000

    notes on trade responses

--- src/model/trade-response.ts
+++ src/model/trade-response.ts
@@ -1,14 +1,16 @@
 import { ActionType } from './enums';
 
 export class TradeResponse {
+  clientOrderId: string;
+  executedQty: string; // asset Amount
   orderId: number;
-  origQty: string;
-  side: string;
-  status: string;
+  origQty: string; // asset Amount (original)
+  side: string; // BUY | SELL
+  status: string; // FILLED | ??
   constructor(
-    public price: number,
-    public symbol: string,
-    public cummulativeQuoteQty: string,
+    public price: number, // get from ticker
+    public symbol: string, // get from ticker
+    public cummulativeQuoteQty: string, // currency amount
     public action: ActionType
   ) {
     action === ActionType.Long ? (this.side = 'BUY') : (this.side = 'SELL');
