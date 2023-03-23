c1282436d3d0ea75133e6740191fd4a039096406
Nathan Adams <npadams21@gmail.com>
2023-03-23 12:24:45 +0000

    add dtprocessed

--- src/model/indicator.ts
+++ src/model/indicator.ts
@@ -25,6 +25,7 @@ export abstract class Indicator {
   trend: any;
   manualUpdate: boolean = false;
   movingAverage: number;
+  dtProcessed: Date;
   resultEntity: { high: number; low: number; average: number } = { high: 0, low: 10000000, average: 0 };
 
   constructor(public settings: IndicatorSettings) {
@@ -50,30 +51,5 @@ export abstract class Indicator {
   percentageDifference(newNumber, originalNumber) {
     const increase = ((newNumber - originalNumber) / originalNumber) * 100;
     return increase;
-  }
-
-  round(tickSize) {
-    var precision = 100000000;
-    var t = this.getPrecision(tickSize);
-
-    if (Number.isInteger(t)) precision = Math.pow(10, t);
-
-    this.result *= precision;
-    this.result = Math.floor(this.result);
-    this.result = this.result /= precision;
-
-    // https://gist.github.com/jiggzson/b5f489af9ad931e3d186
-    // amount = scientificToDecimal(amount);
-  }
-  // Effectively counts the number of decimal places, so 0.001 or 0.234 results in 3
-  getPrecision(tickSize) {
-    if (!isFinite(tickSize)) return 0;
-    var e = 1,
-      p = 0;
-    while (Math.round(tickSize * e) / e !== tickSize) {
-      e *= 10;
-      p++;
-    }
-    return p;
   }
 }
