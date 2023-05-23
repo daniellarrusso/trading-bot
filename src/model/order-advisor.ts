f4437e54783ffe9cf6f715934b88635eb0dd1443
Nathan Adams <npadams21@gmail.com>
2023-05-23 15:45:18 +0100

    order advisor created

---
+++ src/model/order-advisor.ts
@@ -0,0 +1,22 @@
+import { Advisor } from './advisor';
+import { Side, ordertypes } from './literals';
+import { TradeResponse } from './trade-response';
+
+export class OrderAdvisor extends Advisor {
+    type: string;
+    trade(price?: number, side?: Side): Promise<TradeResponse> {
+        throw new Error('Method not implemented.');
+    }
+    end(closingPrice: any) {
+        throw new Error('Method not implemented.');
+    }
+    notifyTelegramBot(message: string): void {
+        throw new Error('Method not implemented.');
+    }
+    addProfitResults(close: number, lastBuy: TradeResponse) {
+        throw new Error('Method not implemented.');
+    }
+    protected setup(orderType: ordertypes) {
+        throw new Error('Method not implemented.');
+    }
+}
