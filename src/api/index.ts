4199b3971ffc1a8229e00ca3f0c3a289eca6295d
Nathan Adams <npadams21@gmail.com>
2023-07-03 13:40:52 +0100

    add cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import tradesRoute from './routes/trades';
 import symbolsRoute from './routes/symbols';
+import cors from 'cors';
 
 const PORT = 8000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/trades', tradesRoute);
 app.use('/symbols', symbolsRoute);
 
