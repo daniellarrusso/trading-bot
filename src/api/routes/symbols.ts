ed04f891fb37474e9dda9b37cbc72f796e97b577
Nathan Adams <npadams21@gmail.com>
2024-09-09 19:42:35 +0100

    remove prisma

--- src/api/routes/symbols.ts
+++ src/api/routes/symbols.ts
@@ -1,11 +1,10 @@
 import { Request, Response, Router } from 'express';
 import { SymbolModel } from '../../db/symbol';
-import prisma from '../../../prisma/client';
 
 const route = Router();
 
 route.get('/', async (req: Request, res: Response) => {
-    const result = await prisma.symbols.findMany();
+    const result = await SymbolModel.find();
     res.json(result).status(200);
 });
 
