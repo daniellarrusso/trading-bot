b581866b792be139cb1b59683d58e57e6abe4232
Nathan Adams <npadams21@gmail.com>
2026-03-25 22:36:55 +0000

    fix: get binance bot working again

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,47 @@
-import { Ticker } from './model/ticker';
-import { ActionType, AdvisorType } from './model/enums';
-import './utilities/extensions';
-import { Trader } from './services/trader-service';
-import { Strat } from './model/interfaces/strat';
-import connectApi from './api';
-import { KrakenService } from './services/kraken-service';
-import 'dotenv/config';
-import { HeikinAshiStrategy } from './strategies/heikin-ashi.strategy';
+import { Ticker } from "./model/ticker";
+import { ActionType, AdvisorType } from "./model/enums";
+import "./utilities/extensions";
+import { Trader } from "./services/trader-service";
+import { Strat } from "./model/interfaces/strat";
+import connectApi from "./api";
+import { KrakenService } from "./services/kraken-service";
 
+import { HeikinAshiStrategy } from "./strategies/heikin-ashi.strategy";
+import { BinanceService } from "./services/binance-service";
+import "dotenv/config";
 
 const trader = Trader.getInstance();
 
 async function loadStrategy() {
-    trader.addStrategy([
-        new HeikinAshiStrategy(
-            new KrakenService(new Ticker('BTC', 'GBP', ActionType.Long, '1h', 100)),
-            AdvisorType.paper
-        ),
-    ]);
+  trader.addStrategy([
+    new HeikinAshiStrategy(
+      new BinanceService(new Ticker("BTC", "USDT", ActionType.Long, "1h", 100)),
+      AdvisorType.paper,
+    ),
+  ]);
 
-    await setup();
+  await setup();
 }
 
 async function setup() {
-    const tickers = trader.strategies.length;
-    for (let i = 0; i < tickers; i++) {
-        const strategy: Strat = trader.strategies[i];
-        const history = await strategy.exchange.getHistory(strategy.exchange.ticker);
-        await strategy.loadHistory(history); // backtesting takes place inside strat
-    }
-    getLatest(tickers);
+  const tickers = trader.strategies.length;
+  for (let i = 0; i < tickers; i++) {
+    const strategy: Strat = trader.strategies[i];
+    const history = await strategy.exchange.getHistory(
+      strategy.exchange.ticker,
+    );
+    await strategy.loadHistory(history); // backtesting takes place inside strat
+  }
+  getLatest(tickers);
 }
 
 function getLatest(tickers: any) {
-    for (let i = 0; i < tickers; i++) {
-        const strategy: Strat = trader.strategies[i];
-        strategy.exchange.getOHLCLatest(strategy.exchange.ticker, async (c) => {
-            await strategy.update(c);
-        });
-    }
+  for (let i = 0; i < tickers; i++) {
+    const strategy: Strat = trader.strategies[i];
+    strategy.exchange.getOHLCLatest(strategy.exchange.ticker, async (c) => {
+      await strategy.update(c);
+    });
+  }
 }
 
 loadStrategy();
